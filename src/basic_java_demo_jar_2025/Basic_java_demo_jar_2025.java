0edebc6317c9c8e477c7ab689e2a00488eb575af
codebeforebreakfast <gitapulousrex@gmail.com>
2025-09-12 15:48:30 -0400

    Added simple java project

---
+++ src/basic_java_demo_jar_2025/Basic_java_demo_jar_2025.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package basic_java_demo_jar_2025;
+
+/**
+ * 
+ */
+public class Basic_java_demo_jar_2025 {
+
+	/**
+	 * 
+	 */
+	public Basic_java_demo_jar_2025() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module basic_java_demo_jar_2025 {
+}
