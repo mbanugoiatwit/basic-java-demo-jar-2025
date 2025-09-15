59212132280eb5f7ecf28c3702623190d692caf5
imban <imban@10.220.46.202>
2025-09-15 15:22:24 -0500

    issue with variables/array?

--- src/basic_java_demo_jar_2025/Basic_java_demo_jar_2025.java
+++ src/basic_java_demo_jar_2025/Basic_java_demo_jar_2025.java
@@ -21,6 +21,11 @@ public class Basic_java_demo_jar_2025 {
 	public static void main(String[] args) {
 		System.out.println("Helloworld");
 
+		x = [0,1,2,3,4];
+		y1 = x * 2;
+		y2 = x*x;
+		
+		System.out.print("x values: " + x + ", y1 = 2x values: " + y1 + ", y2 = x^2 values: " + y2);
 	}
 
 }
